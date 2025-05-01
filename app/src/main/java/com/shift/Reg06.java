e26bb4a17e4379acd74537dd5cf76496972df1ab
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-05-01 19:45:28 +0100

    reg06

---
+++ app/src/main/java/com/shift/Reg06.java
@@ -0,0 +1,35 @@
+package com.shift;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.shift.R;
+
+public class Reg06 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_reg06);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button nextActivityButton = findViewById(R.id.next);
+        Button goBackButton = findViewById(R.id.back);
+
+        nextActivityButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Reg06.this, Reg06.class);
+        });
+    }
+}
