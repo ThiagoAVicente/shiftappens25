2e32ecbc33a45a54b7e7d30b4d2246225feba07b
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-05-01 22:45:03 +0100

    data reg05

--- app/src/main/java/com/shift/Reg05.java
+++ app/src/main/java/com/shift/Reg05.java
@@ -1,8 +1,10 @@
 package com.shift;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Reg05 extends AppCompatActivity {
+
+    private int radioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +27,23 @@ public class Reg05 extends AppCompatActivity {
             return insets;
         });
 
+        SharedPreferences prefs = getSharedPreferences("preferences", MODE_PRIVATE);
+        radioID = prefs.getInt("radioID", -1);
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
         Button nextActivityButton = findViewById(R.id.next);
         Button goBackButton = findViewById(R.id.back);
 
+        radioGroup.check(radioID);
+
         nextActivityButton.setOnClickListener(v -> {
+
+            radioID = radioGroup.getCheckedRadioButtonId();
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("radioID", radioID);
+            editor.apply();
+
             Intent intent = new Intent(Reg05.this, Reg04.class);
             startActivity(intent);
         });
