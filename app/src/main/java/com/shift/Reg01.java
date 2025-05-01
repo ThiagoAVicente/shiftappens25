d35396f70c5d60b18a1b7a7d2c408406e1d40f2f
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-05-01 22:10:01 +0100

    saved data on reg01 and reg02

--- app/src/main/java/com/shift/Reg01.java
+++ app/src/main/java/com/shift/Reg01.java
@@ -1,8 +1,10 @@
 package com.shift;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,23 +14,42 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Reg01 extends AppCompatActivity {
 
+    private String nameInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_reg01);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        EditText name = findViewById(R.id.name);
+
+        SharedPreferences prefs = getSharedPreferences("preferences", MODE_PRIVATE);
+        nameInput = prefs.getString("nameInput", "");
+        name.setText(nameInput);
+
         Button nextActivityButton = findViewById(R.id.next);
 
         nextActivityButton.setOnClickListener(v -> {
+
+            nameInput = name.getText().toString();
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("nameInput", nameInput);
+            editor.apply();
+
             Intent intent = new Intent(Reg01.this, Reg02.class);
             startActivity(intent);
         });
+    }
 
+    public String getNameInput() {
+        return nameInput;
     }
 }
--- app/src/main/java/com/shift/Reg02.java
+++ app/src/main/java/com/shift/Reg02.java
@@ -1,8 +1,10 @@
 package com.shift;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Reg02 extends AppCompatActivity {
+
+    private int day;
+    private int month;
+    private int year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +29,29 @@ public class Reg02 extends AppCompatActivity {
             return insets;
         });
 
+        SharedPreferences prefs = getSharedPreferences("preferences", MODE_PRIVATE);
+        day = prefs.getInt("date_day", 1);
+        month = prefs.getInt("date_month", 5);
+        year = prefs.getInt("date_year", 2025);
+
         Button nextActivityButton = findViewById(R.id.next);
         Button goBackButton = findViewById(R.id.back);
+        DatePicker datePicker = findViewById(R.id.datePicker);
+
+        datePicker.updateDate(year, month, day);
 
         nextActivityButton.setOnClickListener(v -> {
+
+            day = datePicker.getDayOfMonth();
+            month = datePicker.getMonth();
+            year = datePicker.getYear();
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("date_day", day);
+            editor.putInt("date_month", month);
+            editor.putInt("date_year", year);
+            editor.apply();
+
             Intent intent = new Intent(Reg02.this, Reg03.class);
             startActivity(intent);
         });
@@ -35,7 +60,5 @@ public class Reg02 extends AppCompatActivity {
             Intent intent = new Intent(Reg02.this, Reg01.class);
             startActivity(intent);
         });
-
-
     }
 }
