ab199002429054532709f92fa344024281ba5d1f
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-05-01 22:31:55 +0100

    saved data on reg03

--- app/src/main/java/com/shift/Reg03.java
+++ app/src/main/java/com/shift/Reg03.java
@@ -1,8 +1,10 @@
 package com.shift;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Reg03 extends AppCompatActivity {
+
+    private Float height;
+    private Float weight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +28,29 @@ public class Reg03 extends AppCompatActivity {
             return insets;
         });
 
+        SharedPreferences prefs = getSharedPreferences("preferences", MODE_PRIVATE);
+        height = prefs.getFloat("height", 0);
+        weight = prefs.getFloat("weight", 0);
+
+        EditText inputHeight = findViewById(R.id.height);
+        EditText inputWeight = findViewById(R.id.weight);
         Button nextAvtivityButton = findViewById(R.id.next);
         Button goBackButton = findViewById(R.id.back);
 
+        inputHeight.setText(String.valueOf(height));
+        inputWeight.setText(String.valueOf(weight));
+
         nextAvtivityButton.setOnClickListener(v -> {
+
+            height = Float.valueOf(inputHeight.getText().toString());
+            weight = Float.valueOf(inputWeight.getText().toString());
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putFloat("height", height);
+            editor.putFloat("weight", weight);
+            editor.apply();
+
+
             Intent intent = new Intent(Reg03.this, Reg04.class);
             startActivity(intent);
         });
