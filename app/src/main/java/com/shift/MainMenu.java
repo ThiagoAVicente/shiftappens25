59b173620697bc861fc9e10dedb33a079c75f194
vcnt <thiago.vicente@ua.pt>
2025-05-02 11:14:14 +0100

    added o goat

--- app/src/main/java/com/shift/MainMenu.java
+++ app/src/main/java/com/shift/MainMenu.java
@@ -3,6 +3,7 @@ package com.shift;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +19,20 @@ public class MainMenu extends AppCompatActivity {
         Button registerButton = findViewById(R.id.registerButton);
         Button saveToCardButton = findViewById(R.id.saveCardButton);
         Button optionsButton = findViewById(R.id.optionsButton);
+
+        NfcControl nfcControl = NfcControl.create(this);
+
+        if (nfcControl != null) {
+            Log.d("MainMenu", "NfcControl created successfully");
+            // Optionally check NFC availability
+            if (nfcControl.isNfcAvailable()) {
+                Log.d("MainMenu", "NFC is available and enabled");
+            } else {
+                Log.d("MainMenu", "NFC is not available or disabled");
+            }
+        } else {
+            Log.e("MainMenu", "Device does not support NFC");
+        }
 
         registerButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainMenu.this, Reg01.class);
